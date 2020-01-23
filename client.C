fe01df2f5dd2d70728b08f4f3d42b4a785195c41
bboberg <ben-lukas@web.de>
2020-01-23 14:58:25 +0100

    Implementation of All, Rand, Advanced 1.0

--- client.C
+++ client.C
@@ -19,6 +19,7 @@
 using namespace std;
 
 int shootALL(TCPclient *ptrC);
+int Shootrand(TCPclient *ptrC);
 int shootAdvanced(TCPclient *ptrC);
 int oneShoot(TCPclient *ptrC, int x, int y);
 float mittelwertshootAdvanced(TCPclient *c, int n);
@@ -33,9 +34,23 @@ int main() {
 	//connect to host
 	c.conn(host , 2022);
 
-	cout << "Implementierung der simplen Reihe für Reihe Strategie:" << endl;
+	cout << endl << "Implementierung der simplen Reihe für Reihe Strategie:" << endl;
 
-	shtNmb = mittelwertshootAdvanced(&c,90000);
+	for(int i=1;i<=10;i++){							//Change number of process
+		shtNmb = shootALL(&c);
+		cout << i << ".: " << shtNmb << endl;
+	}
+
+	cout << endl << "Implementierung der Zufallsbeschuss-Strategie:" << endl;
+
+	for(int i=1;i<=10;i++){							//Change number of process
+		shtNmb = Shootrand(&c);
+		cout << i << ".: " << shtNmb << endl;
+	}	
+
+	cout << endl << "Implementierung der alle-zwei-Felder-Strategie:" << endl;
+
+	shtNmb = mittelwertshootAdvanced(&c,10);		//Change number of process
 	cout << "MEAN: " << shtNmb << endl;
 
 }
@@ -45,7 +60,7 @@ float mittelwertshootAdvanced(TCPclient *c, int n)
 	int r;
 	int min=100;
 	int max=0;
-	for(int i=0;i<n;i++)
+	for(int i=1;i<=n;i++)
 	{
 		r = shootAdvanced(c);
 		if(r<min)
@@ -57,10 +72,10 @@ float mittelwertshootAdvanced(TCPclient *c, int n)
 			max=r;
 		}
 		shtNmb = shtNmb + r;
-		cout << r << endl;
+		cout << i << ".: " << r << endl;
 	}
 	shtNmb=(float)shtNmb/n;
-	cout<<"Minimum"<<min<<"Maximum"<<max<<endl;
+	cout << endl << "Minimum: "<<min<<"  Maximum: "<<max<<endl;
 	return shtNmb;
 }
 
@@ -85,26 +100,66 @@ int shootALL(TCPclient *ptrC){
 	ptrC-> sendData(string("NEWGAME")); //start new Game
 	msg = ptrC->receive(32);			//response
 
-	for(int x=1; x<=10; x++){
+		for(int x=1; x<=10; x++){
 		
-		for(int y=1; y<=10; y++){
-			if(sstm!=NULL){
-				delete sstm;
+			for(int y=1; y<=10; y++){
+				if(sstm!=NULL){
+					delete sstm;
+				}
+
+				r = oneShoot(ptrC, x,y);
+				count++;
+
+				if(r == TASK3::GAME_OVER){
+					break;
+				};
 			}
-
-			r = oneShoot(ptrC, x,y);
-			count++;
-
 			if(r == TASK3::GAME_OVER){
 				break;
 			};
 		}
-		if(r == TASK3::GAME_OVER){
+		return count;
+}
+
+int Shootrand(TCPclient *ptrC){
+
+	srand(time(NULL));
+	stringstream *sstm = NULL;
+	string msg;
+	int r, count=0;
+
+	ptrC-> sendData(string("NEWGAME")); //start new Game
+	msg = ptrC->receive(32);			//response
+
+	while(msg.compare(0,5, "RES[4") != 0){
+		if(sstm!=NULL){
+				delete sstm;
+			}
+
+		int x = (rand() % 10)+1;
+		int y = (rand() % 10)+1;
+
+		sstm = new stringstream();
+		*sstm << "COORD["<<x<<","<<y<<"]";
+		ptrC->sendData(sstm->str());
+		msg = ptrC -> receive(32);
+		sscanf(msg.c_str(), "RES[%d]", &r);
+
+		if(r <= 2){
+			count++;
+		}
+
+		else if(r == 4){
 			break;
+		}
+
+		else{
+			cout << "Unknown: " << msg << endl;
 		};
 	}
 	return count;
 }
+
 int shootAdvanced(TCPclient *ptrC){
 
 	stringstream *sstm = NULL;
